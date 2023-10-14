77494d251bd0ef8b902f305206e49a0439ab1092
Uchenna Oko <uche4newton1981@gmail.com>
2023-10-14 15:53:40 +0100

    Updating console module

--- console.py
+++ console.py
@@ -2,11 +2,35 @@
 """A command interpreter for the AirBnB project"""
 
 import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    """A class that inherits from cmd.Cmd"""
+    """A class that inherits from cmd.Cmd
+    Console command class.
+    """
 
     prompt = "(hbnb) "
+    o_dic = {
+        'BaseModel': BaseModel,
+        'User': User,
+    }
+    cmds = ['create']
+    
+    def do_create(self, arg):
+        """Create command creates new instance,
+        saves it to JSON file & prints the id.
+        Usage: create <class>
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.o_dic.keys():
+            print("** class doesn't exist **")
+        else:
+            b_mod = HBNBCommand.o_dic[arg]()
+            print(b_mod.id)
+            b_mod.save()
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
