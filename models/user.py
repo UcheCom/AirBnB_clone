11cda7e2a59eb596ba2fe8cb3dc54a1d86627a42
Uchenna Oko <uche4newton1981@gmail.com>
2023-10-14 16:26:21 +0100

    Updating Models Package

--- models/user.py
+++ models/user.py
@@ -6,15 +6,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ class user inherits from the BaseModel. 
-    
+    """ class user inherits from the BaseModel.
+
     Attributes:
         email: string - The email of the user.
         password: string - The password of the user.
         first_name: string - The first name of the user.
         last_name: string - The last name of the user.
     """
-    
+
     email = ""
     password = ""
     first_name = ""
